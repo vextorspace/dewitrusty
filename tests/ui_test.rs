d30ea43121c70fd087a364f611ddccd65dfa3584
Douglas Ronne <vextorspace@gmail.com>
2024-11-28 10:19:09 -0800

    removed extra lines

--- tests/ui_test.rs
+++ tests/ui_test.rs
@@ -5,8 +5,6 @@ use dewitrusty::ui::slint_exports::AppWindow;
 
 #[test]
 fn test_title_correct() {
-
-
     let app = AppWindow::new().unwrap();
     Initializer::default().run(&app);
     assert_eq!(app.get_window_title(), "Dew-It");
