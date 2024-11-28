4deaf8ff86e08a4c33f2a63625f4775e248f94b0
Douglas Ronne <vextorspace@gmail.com>
2024-11-28 05:49:40 -0800

    list comes from rust

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let ui = AppWindow::new()?;
 
-    let items: Vec<ListItem> = ui.get_items().iter().collect();
+    let items = slint::VecModel::default();
+
+    items.push(ListItem{text: "Item 1".into()});
+    items.push(ListItem{text: "Item 2".into()});
+    items.push(ListItem{text: "Added Item".into()});
+
+    ui.set_items(std::rc::Rc::new(items).into());
 
     ui.run()?;
 
