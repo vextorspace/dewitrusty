781ab430cb422b50fb038241fce546c279817653
Douglas Ronne <vextorspace@gmail.com>
2024-11-30 10:53:51 -0800

    items can be created and always have an id

--- src/data/item_data.rs
+++ src/data/item_data.rs
@@ -3,12 +3,32 @@ pub struct ItemData {
     pub id: String,
 }
 
-
 impl ItemData {
     pub fn new(text: &str) -> ItemData {
         ItemData {
             text: text.to_string(),
             id: uuid::Uuid::new_v4().to_string(),
+        }
+    }
+
+    pub fn default() -> Self {
+        ItemData {
+            text: "".to_string(),
+            id: uuid::Uuid::new_v4().to_string(),
+        }
+    }
+
+    pub fn with_text(&self, text: &str) -> Self {
+        ItemData {
+            text: text.to_string(),
+            id: self.id.clone(),
+        }
+    }
+
+    pub fn with_id(&self, id: &str) -> Self {
+        ItemData {
+            text: self.text.clone(),
+            id: id.to_string(),
         }
     }
 }
@@ -18,14 +38,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn item_has_text() {
-        let item = ItemData::new("foo");
-        assert_eq!(item.text, "foo");
+    fn new_item_has_text() {
+        let text = "::THE TEXT::";
+        let item = ItemData::new(text);
+        assert_eq!(item.text, text);
     }
 
     #[test]
-    fn item_has_id() {
+    fn new_item_has_id() {
         let item = ItemData::new("foo");
         assert!(item.id.len() > 0);
+    }
+
+    #[test]
+    fn item_can_be_built_with_text_and_id() {
+        let text = "::THE TEXT::";
+        let id = "::THE ID::";
+        let item = ItemData::default().with_text(text).with_id(id);
+
+        assert_eq!(item.text, text);
+        assert_eq!(item.id, id);
     }
 }
