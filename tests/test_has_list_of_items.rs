15b8fc958a551324f2fd63aad0d7fedcbeaf7158
Douglas Ronne <vextorspace@gmail.com>
2024-12-01 11:51:42 -0800

    removed dead-end visitor methodology - not feasible in rust

--- tests/test_has_list_of_items.rs
+++ tests/test_has_list_of_items.rs
@@ -1,19 +1,4 @@
-use dewitrusty::fltk::child_visitor::{ChildVisitor, VisitOperation};
-use dewitrusty::ui::window_builder::WindowBuilder;
-use fltk::app::App;
 use fltk::prelude::WidgetExt;
-use fltk::window::Window;
-
-#[test]
-fn there_exists_hpack_child() {
-    let app = App::default();
-    let wind: Window = WindowBuilder::new().build();
-
-    let mut finder = ItemListFinder::new();
-    ChildVisitor::new(Box::new(&wind)).visit(&mut finder);
-
-    assert_eq!(finder.item.unwrap().label(), "DewIt Now");
-}
 
 struct ItemListFinder {
     item: Option<Box<dyn WidgetExt>>,
@@ -25,8 +10,4 @@ impl ItemListFinder {
             item: None,
         }
     }
-}
-
-impl<'a> VisitOperation<'a> for ItemListFinder {
-    fn visit(&mut self, child: Box<&'a dyn WidgetExt>) {}
 }
